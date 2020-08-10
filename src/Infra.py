54eab43e4612ed7a297ea0b5e4405e0ac3f06cc6
Rahul Gopinath <rahul@gopinath.org>
2020-08-10 07:46:45 -0700

    update

--- src/Infra.py
+++ src/Infra.py
@@ -111,5 +111,22 @@ def main(gf_fbjson, bug_fn, pred, results_dir='results', max_checks=A.MAX_CHECKS
 
     cfault = F.find_charecterizing_node(a_mintree)
     cg, cs = F.atleast_one_fault_grammar(meta['[grammar]'], meta['[start]'], cfault, 1)
+    cg = F.complete(cg, cs)
     with open('./%s/%s.json' % (results_dir, A.NAME + '_atleast_one_fault_g'), 'w+') as f:
         print(json.dumps({ '[start]': cs, '[grammar]': cg}), file=f)
+
+    cg, cs = F.atmost_one_fault_grammar(meta['[grammar]'], meta['[start]'], cfault, 1)
+    cg = F.complete(cg, cs)
+    with open('./%s/%s.json' % (results_dir, A.NAME + '_atmost_one_fault_g'), 'w+') as f:
+        print(json.dumps({ '[start]': cs, '[grammar]': cg}), file=f)
+
+    cg, cs = F.exactly_one_fault_grammar(meta['[grammar]'], meta['[start]'], cfault, 1)
+    cg = F.complete(cg, cs)
+    with open('./%s/%s.json' % (results_dir, A.NAME + '_exactly_one_fault_g'), 'w+') as f:
+        print(json.dumps({ '[start]': cs, '[grammar]': cg}), file=f)
+
+    cg, cs = F.no_fault_grammar(meta['[grammar]'], meta['[start]'], cfault, 1)
+    cg = F.complete(cg, cs)
+    with open('./%s/%s.json' % (results_dir, A.NAME + '_no_fault_g'), 'w+') as f:
+        print(json.dumps({ '[start]': cs, '[grammar]': cg}), file=f)
+
